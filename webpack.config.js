028c493f1801ec5a33890d5a8e44f1af61fe31bc
VErgasheva <vikser587@gmail.ru>
2025-05-17 01:14:49 +0500

    4

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: new URL('./dist', import.meta.url).pathname,
-    clean: true, 
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   mode: 'development',
   module: {
@@ -23,6 +29,24 @@ export default {
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'index.html', to: '' },
+        { from: 'eslint.config.js', to: '' },
+        { from: 'webpack.config.js', to: '' },
+        { from: 'vercel.json', to: '' },
+        { from: 'package.json', to: '' },
+        { from: 'src/counter.js', to: 'src/' },
+        { from: 'src/i18n.js', to: 'src/' },
+        { from: 'src/index.js', to: 'src/' },
+        { from: 'src/logic.js', to: 'src/' },
+        { from: 'src/main.js', to: 'src/' },
+        { from: 'src/rssParser.js', to: 'src/' },
+        { from: 'src/styles.scss', to: 'src/' },
+        { from: 'src/ui.js', to: 'src/' },
+        { from: 'src/view.js', to: 'src/' },
+      ],
     }),
   ],
 };
