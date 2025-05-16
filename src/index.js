c902017a0a75c76262a7dc298ef16d09d18b8d19
VErgasheva <vikser587@gmail.ru>
2025-05-16 12:25:52 +0500

    2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import  './styles.scss';
 import  'bootstrap';
+import initView from './view.js';
+import initLogic from './logic.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = {
+    form: document.getElementById('main_form'),
+    input: document.getElementById('url_input'),
+    infoText: document.getElementById('info_text'),
+  };
+
+  const state = {
+    feeds: [],
+    form: {
+      valid: true,
+      error: null,
+    },
+  };
+
+  initView(elements, state);
+  initLogic(elements, state);
+});
