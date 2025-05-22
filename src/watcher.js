a1b67a542d59fdf7a7f0d1dcf9c4e166253de2fe
VErgasheva <vikser587@gmail.ru>
2025-05-22 11:12:08 +0500

    7

--- src/watcher.js
+++ src/watcher.js
@@ -1,31 +1,34 @@
 import onChange from 'on-change';
 import { renderFeeds, renderPosts, showInfo, showError } from './ui.js';
 
-export default (state, elements) => onChange(state, function (path, value) {
-  if (path === 'feeds') {
-    renderFeeds(state.feeds);
-    elements.feedsBlock.classList.remove('d-none');
-  }
-  if (path === 'posts') {
-    renderPosts(state.posts, state);
-    elements.postsBlock.classList.remove('d-none');
-  }
-  if (path === 'form.error') {
-    if (value) {
-      elements.input.classList.add('is-invalid');
-      elements.infoText.textContent = value;
-      elements.infoText.classList.remove('d-none');
-      elements.infoText.classList.remove('text-success');
-      elements.infoText.classList.add('text-danger');
-    } else {
-      elements.input.classList.remove('is-invalid');
-      elements.infoText.classList.add('d-none');
-      elements.infoText.textContent = '';
-    }
-  }
-  if (path === 'form.valid' && state.form.valid && !state.form.error) {
-    elements.input.classList.remove('is-invalid');
-    elements.infoText.classList.add('d-none');
-    elements.infoText.textContent = '';
+export default (state, elements) => onChange(state, (path, value) => {
+  switch (path) {
+    case 'feeds':
+      renderFeeds(state.feeds);
+      elements.feedsBlock.classList.remove('d-none');
+      break;
+
+    case 'posts':
+      renderPosts(state.posts, state);
+      elements.postsBlock.classList.remove('d-none');
+      break;
+
+    case 'form.error':
+    case 'form.valid':
+      if (state.form.valid) {
+        elements.input.classList.remove('is-invalid');
+        elements.infoText.classList.add('d-none');
+        elements.infoText.textContent = '';
+      } else {
+        elements.input.classList.add('is-invalid');
+        elements.infoText.textContent = state.form.error || value;
+        elements.infoText.classList.remove('d-none');
+        elements.infoText.classList.remove('text-success');
+        elements.infoText.classList.add('text-danger');
+      }
+      break;
+
+    default:
+      break;
   }
 });
